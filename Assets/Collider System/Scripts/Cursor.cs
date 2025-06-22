2a0a193f8aae98820a7ed385048076ef6449ced8
SoltidueMarquez <2630388173@qq.com>
2025-06-22 16:20:26 +0800

    完结.波函数坍缩暂时做不了

--- Assets/Collider System/Scripts/Cursor.cs
+++ Assets/Collider System/Scripts/Cursor.cs
@@ -1,4 +1,5 @@
-﻿using Grid_Generator;
+﻿using System.Linq;
+using Grid_Generator;
 using UnityEngine;
 using Grid = Grid_Generator.Grid;
 
@@ -6,13 +7,40 @@ namespace Collider_System
 {
     public class Cursor : MonoBehaviour
     {
-        public void UpdateCursor(RaycastHit raycastHit,RaycastHitType raycastHitType, VertexY selected, VertexY target)
+        private void Awake()
+        {
+            transform.localScale = Vector3.one * 1.1f;
+        }
+
+        public void UpdateCursor(RaycastHit raycastHit, RaycastHitType raycastHitType, VertexY selected, VertexY target)
         {
             GetComponent<MeshFilter>().mesh.Clear();
-            if (raycastHitType == RaycastHitType.none) return;
-            Vertex vertex = target.vertex;
-            GetComponent<MeshFilter>().mesh = vertex.CreateMesh();
-            transform.position = target.worldPosition + Vector3.down * (Grid.cellHeight * (0.5f));
+            if (raycastHitType == RaycastHitType.ground)
+            {
+                Vertex vertex = target.vertex;
+                GetComponent<MeshFilter>().mesh = vertex.CreateMesh();
+                transform.position = target.worldPosition + Vector3.down * (Grid.cellHeight * (0.5f));
+            }
+            else if (raycastHitType == RaycastHitType.top)
+            {
+                Vertex vertex = selected.vertex;
+                GetComponent<MeshFilter>().mesh = vertex.CreateMesh();
+                transform.position = selected.worldPosition + Vector3.up * (Grid.cellHeight * 0.6f);
+            }
+            else if (raycastHitType == RaycastHitType.bottom)
+            {
+                Vertex vertex = selected.vertex;
+                GetComponent<MeshFilter>().mesh = vertex.CreateMesh();
+                GetComponent<MeshFilter>().mesh.triangles =
+                    GetComponent<MeshFilter>().mesh.triangles.Reverse().ToArray();
+                transform.position = selected.worldPosition + Vector3.down * (Grid.cellHeight * 0.6f);
+            }
+            else if (raycastHitType == RaycastHitType.side)
+            {
+                Vertex vertex = selected.vertex;
+                GetComponent<MeshFilter>().mesh = raycastHit.transform.GetComponent<MeshCollider>().sharedMesh;
+                transform.position = selected.worldPosition;
+            }
         }
     }
 }
